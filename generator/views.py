3f8e72f81bb333eb90d4bae2d204c4a7cce6b3da
alxanderapollo <alxanderapollo@gmail.com>
2021-03-10 21:19:37 -0500

    added templates which will be where i will customize websites to my liking, the templates folder has the name of the generator folder app

--- generator/views.py
+++ generator/views.py
@@ -6,8 +6,12 @@ from django.http import HttpResponse
 # the user will request to go to the home page
 # we need to provide a response in the form of HTTP
 #this will take the user to a page that will say hello there friend 
+
+#render is a function from django that allows us to pass back a template, that turns into a http response 
+
+#password  is dictionar key map that can be referenced inside of the home.html file
 def home(request):
-    return HttpResponse('Hello there friend')
+    return render(request,'generator/home.html', {'password':'apples'})
 
 #eggs page
 def eggs(request):
