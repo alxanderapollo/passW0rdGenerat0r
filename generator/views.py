5a527e1485c2010618256d3dc4f6beca9ce21489
alxanderapollo <alxanderapollo@gmail.com>
2021-03-11 20:46:11 -0500

    added special characters, numbers and letters, user can now request forom 6- 15 character long passwords, and they are generated randomly

--- generator/views.py
+++ generator/views.py
@@ -21,8 +21,26 @@ def password(request):
     thePassWord = ''
     #takes this string and breaks it up into an array
     characters = list('abcdefghijklmnopqrstuvwxyz')
-    #length of the password
-    length = 10
+    #get the length requested by the user
+        #integers conversion, gets the value from length passed on by the user in the following page
+        #becomes the overall length of the new string
+    
+    #UPPER CASE LETTERS
+    #check to see if the user is requesting uppercase letters
+        #if they do add the capital letters to the already lower case list
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))     
+    #SPECIAL LETTERS
+    #add special letters to the list
+    if request.GET.get('Special'):
+        characters.extend(list('!@#$%^&*()_+-\[];?>.<,~'))
+    #Numbers
+    if request.GET.get('numbers'):
+        characters.extend(list('0123456789'))
+        
+          
+        
+    length = int(request.GET.get('length',12)) #12 represents the default value
     for x in range(length):
         thePassWord += random.choice(characters)
     
